cdc78048eb6a73c5169811aa93b5eacef3aff8a9
michailid <michailidoumaria224@gmail.com>
2026-08-10 20:46:39 +0300

    init commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Stopwatch } from './stopwatch/stopwatch';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Stopwatch],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/stopwatch/stopwatch.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stopwatch',
+  imports: [],
+  templateUrl: './stopwatch.html',
+  styleUrl: './stopwatch.css',
+})
+export class Stopwatch {}
