4abeabc84f76e2de9175a71b208e1f0f84037541
michailid <michailidoumaria224@gmail.com>
2026-08-22 20:12:01 +0300

    traffic system UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CartSystem } from './cart-system/cart-system';
+import { Traffic } from './traffic/traffic';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CartSystem],
+  imports: [RouterOutlet, Traffic],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
